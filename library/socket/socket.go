2da9fdf9271b538026aa347400bea173fc9ff6c2
ogmj <ogmj98@gmail.com>
2024-06-19 12:48:51 +0900

    [라이브러리] 소켓버퍼의 초기화함수, 피크, 리드, 라이트 함수 추가

--- library/socket/socket.go
+++ library/socket/socket.go
@@ -1,6 +1,7 @@
 package socket
 
 import (
+	"errors"
 	"net"
 )
 
@@ -21,4 +22,37 @@ type TCP struct {
 type Listener struct {
 	ln       net.Listener
 	flagStop bool
+}
+
+func (b *socketBuffer) initSocketBuffer() {
+	b.data = make([]byte, 65536)
+}
+
+func (b *socketBuffer) write(packet []byte) {
+	packetLength := len(packet)
+	if dataLength := copy(b.data[b.offset:], packet); dataLength < packetLength {
+		b.data = append(b.data, packet[dataLength:]...)
+	}
+	b.offset = b.offset + len(packet)
+}
+
+func (b *socketBuffer) peek(size int) ([]byte, error) {
+	if size > b.offset {
+		return nil, errors.New("peek func Packet Overflow")
+	}
+	return b.data[:size], nil
+}
+
+func (b *socketBuffer) read(buffer []byte, size int) error {
+	if size > b.offset {
+		return errors.New("read func Packet Overflow")
+	}
+	if len(buffer) < size {
+		panic("read func panic")
+	}
+	b.offset = b.offset - size
+	copy(buffer, b.data[:size])
+	copy(buffer, b.data[size:])
+
+	return nil
 }
