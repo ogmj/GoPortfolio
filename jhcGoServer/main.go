14bfa9a7a8023bc16e08bee50609c111d2c646ee
ogmj <ogmj98@gmail.com>
2024-06-24 16:54:40 +0900

    [서버] AsyncAccept 내용추가

--- jhcGoServer/main.go
+++ jhcGoServer/main.go
@@ -21,15 +21,20 @@ func initTcpSocketNetwork() {
 	err := jhcGOTcpServer.Listen(9999)
 
 	if err != nil {
-		panic("Failed to initTcpSocketNetword");
+		panic("Failed to initTcpSocketNetword")
 	}
 
-	jhcGOTcpServer.AsyncAccept(func(connection *socket.TCP)) {
+	jhcGOTcpServer.AsyncAccept(func(connection *socket.TCP) {
 		//TODO : 통신 버퍼를 셋하자.
+		buffer := make([]byte, 32768)
 		//TODO : 셋한 통신 버퍼에 TCP 소켓에 있는 값을 리시브한다.
+		err := connection.ConnectionHandler(buffer)
 		//TODO : 에러인 경우 처리하는 함수를 추가한다.
+		if err != nil {
+			connection.Close()
+		}
 		//TODO : 위의 작업을 처리하는 함수는 TCP스트럭트에서 처리하도록 한다.
-	}
+	})
 }
 
 func readString(reader *bufio.Reader) string {
