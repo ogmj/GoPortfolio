4fcbf6441ba82eb3714a8510d9116b3e5654a74e
ogmj <ogmj98@gmail.com>
2024-06-26 11:49:43 +0900

    [서버] 프로젝트에 종속되어야 하는 함수를 프로젝트로 옮김

--- jhcGoServer/main.go
+++ jhcGoServer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"fmt"
 	"library/socket"
 	"os"
@@ -24,17 +25,52 @@ func initTcpSocketNetwork() {
 		panic("Failed to initTcpSocketNetword")
 	}
 
+	//TODO : 비동기 accept함수 구현
 	jhcGOTcpServer.AsyncAccept(func(connection *socket.TCP) {
 		//TODO : 통신 버퍼를 셋하자.
 		buffer := make([]byte, 32768)
 		//TODO : 셋한 통신 버퍼에 TCP 소켓에 있는 값을 리시브한다.
-		err := connection.ConnectionHandler(buffer)
-		//TODO : 에러인 경우 처리하는 함수를 추가한다.
-		if err != nil {
-			connection.Close()
-		}
-		//TODO : 위의 작업을 처리하는 함수는 TCP스트럭트에서 처리하도록 한다.
+		connection.ConnectionHandler(func() {
+			//TODO : TCP통신 특성상 나뉘어 오는 패킷 처리
+			for extractPacket(connection, buffer) != nil {
+				//TODO : 받은 패킷 처리
+				packetReceiver(connection, buffer)
+			}
+		}, func() {
+			//TODO : 에러인 경우 접속을 해제한다.
+			closeTcpSocket(connection)
+		})
 	})
+}
+
+func extractPacket(connection *socket.TCP, buffer []byte) []byte {
+	const headerSize int = 4
+
+	rawHeader, err := connection.Peek(headerSize)
+	if err != nil {
+		return nil
+	}
+	rawSize := rawHeader[2:4]
+	size := binary.LittleEndian.Uint16(rawSize)
+
+	err = connection.Read(buffer, int(size))
+	return nil
+}
+
+func closeTcpSocket(connection *socket.TCP) {
+	connection.Close()
+}
+
+func packetReceiver(connection *socket.TCP, packet []byte) {
+	const headerSize int = 4
+	rawMsg := packet[0:2]
+	msg := binary.LittleEndian.Uint16(rawMsg)
+
+	switch msg {
+	case 10001:
+		fmt.Println("test")
+	}
+
 }
 
 func readString(reader *bufio.Reader) string {
@@ -44,7 +80,7 @@ func readString(reader *bufio.Reader) string {
 }
 
 func main() {
-	fmt.Println("JhcGoServer")
+	fmt.Println("JhcGoServer Start")
 
 	initNetwork()
 
