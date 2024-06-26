4d4c91e044856bbcf94f5915ca576b7d49fc13c8
ogmj <ogmj98@gmail.com>
2024-06-27 00:08:49 +0900

    [클라이언트] Connect 함수 구현 및 Connect성공 및 실패 처리

--- jhcGoClient/server/server.go
+++ jhcGoClient/server/server.go
@@ -1,6 +1,11 @@
 package server
 
-import "library/socket"
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"library/socket"
+)
 
 type Session struct {
 	connection *socket.TCP
@@ -24,9 +29,17 @@ func GetSession() *Session {
 	return &session
 }
 
-//TODO : 연결 함수
-func Connect(ip string, port uint) error {
-	return nil
+// TODO : 연결 함수
+func (s *Session) Connect(ip string, port uint) error {
+	if session.connection.Connect(ip, port) {
+		s.connection.ConnectionHandler(func() {
+			for s.extractPacket(s.connection, buffer) != nil {
+				s.receiver(buffer)
+			}
+		}, s.closer)
+		return nil
+	}
+	return errors.New("Connection Refused")
 }
 
 // TODO : 패킷을 보내는 부분
@@ -34,9 +47,35 @@ func Send() {
 }
 
 // TODO : TCP통신의 특성을 감안하여 여러 개의 스트림에 한 개의 패킷이 올 것을 대비하여 처리
-func extractPacket() {
+func (s *Session) extractPacket(connection *socket.TCP, buffer []byte) []byte {
+	const headerSize int = 4
+	rawHeader, err := connection.Peek(headerSize)
+	if err != nil {
+		return nil
+	}
+
+	rawSize := rawHeader[2:4]
+	size := binary.LittleEndian.Uint16(rawSize)
+
+	err = connection.Read(buffer, int(size))
+	if err != nil {
+		return nil
+	}
+
+	return buffer
 }
 
 // TODO : 패킷을 받는 부분
-func receiver() {
+func (s *Session) receiver(packet []byte) {
+	m := packet[0:2]
+	msg := binary.LittleEndian.Uint16(m)
+	switch msg {
+	case 10000:
+		fmt.Println("test")
+	}
+}
+
+// TODO : 세션을 닫을때 처리하는 부분
+func (s *Session) closer() {
+	s.connection = nil
 }
